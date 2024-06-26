e8cef2526c0b984d9bb6f08483f1aa0869309186
Chenghao Lyu <chenghao@cs.umass.edu>
2024-06-26 15:23:12 -0400

    support RAAL and QF (#31)
    * support RAAL and QF
    
    * variable order
    
    * speedup raal
    
    * support predict
    
    * fix the error of compute WMAPE for val
    
    * reset metrics
    
    * fix
    
    * fix

--- udao/model/module.py
+++ udao/model/module.py
@@ -139,14 +139,27 @@ class UdaoModule(pl.LightningModule):
         )
         return loss
 
-    def on_train_epoch_end(self) -> None:
-        self._shared_epoch_end("train")
+    def predict_step(
+        self, batch: Tuple[Any, th.Tensor], batch_idx: int, dataloader_idx: int = 0
+    ) -> th.Tensor:
+        features, _ = batch
+        return self.model(features)
+
+    def _reset_metrics(self) -> None:
+        for objective in self.objectives:
+            cast(Metric, self.metrics[objective]).reset()
+
+    def on_validation_epoch_start(self) -> None:
+        self._reset_metrics()
 
     def validation_step(self, batch: Tuple[Any, th.Tensor], batch_idx: int) -> None:
         self._shared_step(batch, "val")
 
     def on_validation_epoch_end(self) -> None:
         self._shared_epoch_end("val")
+
+    def on_test_epoch_start(self) -> None:
+        self._reset_metrics()
 
     def test_step(self, batch: Tuple[Any, th.Tensor], batch_idx: int) -> None:
         self._shared_step(batch, "test")
